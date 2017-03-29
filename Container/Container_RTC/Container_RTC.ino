98af6edf8cbbd43b46e1e33535fef3ae7a804540
Zane Gyorko <gyorko48@gmail.com>
2017-03-29 19:16:26 -0400

    Fixed RTC code

--- Container/Container_RTC/Container_RTC.ino
+++ Container/Container_RTC/Container_RTC.ino
@@ -12,6 +12,7 @@
  */
 
 DateTime initialTime;
+bool timeSet;
 RTC_DS3231 rtc;
 
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
@@ -40,42 +41,19 @@ void setup () {
     // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
   }
 
-  DateTime initialTime=rtc.now();
+  bool timeSet=false;
 }
 
 void loop () {
+    if (!timeSet){
+      initialTime=rtc.now();
+      timeSet=true;
+    }
     DateTime currentTime = rtc.now();
     TimeSpan missionDateTime=(currentTime-initialTime);
-    int missionTime=missionDateTime.totalseconds();
+    int missionTime=currentTime.unixtime()-initialTime.unixtime();
     
-    Serial.print(missionTime);
-    
-    /*Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(" (");
-    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-    Serial.print(") ");
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
-    
-    Serial.print(" since midnight 1/1/1970 = ");
-    Serial.print(now.unixtime());
-    Serial.print("s = ");
-    Serial.print(now.unixtime() / 86400L);
-    Serial.println("d");
-    
-    // calculate a date which is 7 days and 30 seconds into the future
-    DateTime future (now + TimeSpan(7,12,30,6));
-    
-    Serial.print(" now + 7d + 30s: ");
-    Serial.print(future.year(), DEC);
-    */
-    
+    Serial.println(missionTime);
+
+    delay(1000);
 }
