aceaa6164296a83681e3128556aa020514980314
lovellkevina <lovellkevina@gmail.com>
2017-03-29 20:04:04 -0400

    magnetometer test

---
+++ Payload/magtest.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		magtest.ino
+ Created:	3/29/2017 7:28:27 PM
+ Author:	kevin
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
