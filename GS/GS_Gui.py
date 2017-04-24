5907c4297a0ae2a7ac716405fa442be1932a1d73
chasefarmer2808 <chasefarmer2808@ufl.edu>
2017-04-23 22:47:54 -0400

    send and revieve data

--- GS/GS_Gui.py
+++ GS/GS_Gui.py
@@ -4,6 +4,11 @@ from PyQt4 import QtGui, QtCore
 
 xbee = serial.Serial('COM3')
 
+
+def read_packet():
+    packet = xbee.readline()
+    print(packet)
+
 class Window(QtGui.QMainWindow):
 
     def __init__(self):
@@ -11,19 +16,25 @@ class Window(QtGui.QMainWindow):
         self.setGeometry(50, 50, 500, 300)
         self.setWindowTitle("GS")
         self.cmd_zero_btn()
+
         self.show()
+
 
     def cmd_zero_btn(self):
         btn = QtGui.QPushButton("Reset Save Data", self)
         btn.clicked.connect(self.send_cmd)
-        #btn.resize(100, 100)
-        #btn.move(100, 100)
-        #self.show()
 
     def send_cmd(self):
         print('here')
         xbee.write('z')
 
+
+
 app = QtGui.QApplication(sys.argv)
 GUI = Window()
+
+timer = QtCore.QTimer()
+timer.timeout.connect(read_packet)
+timer.start(10)
+
 sys.exit(app.exec_())
