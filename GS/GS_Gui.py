6425879ec16a6ee15112cdc513586f0a5e227171
chasefarmer2808 <chasefarmer2808@ufl.edu>
2017-04-24 01:21:48 -0400

    buttons for buzzer cmd

--- GS/GS_Gui.py
+++ GS/GS_Gui.py
@@ -2,13 +2,27 @@ import sys
 import serial
 from PyQt4 import QtGui, QtCore
 
-xbee = serial.Serial('COM3')
+CMD_RELEASE = 'r'
+CMD_BUZZER = 'b'
+CMD_RESET = 'z'
+CMD_LAND = 'l'
 
+xbee = serial.Serial('COM3', timeout=0.1)  #initialize a serial object on the com port
 
+#function read_packet()
+'''
+    this function is called by the timer in order to have pseudo parallelism between
+    drawing the GUI and monitoring the serial port.
+'''
 def read_packet():
-    packet = xbee.readline()
-    print(packet)
+    packet = xbee.readline()  #read chars until '\r\n'
 
+    if packet:
+        print(packet)
+
+'''
+    this is a handler class for PyQt.  It handles drawing the window, buttons, and graphs
+'''
 class Window(QtGui.QMainWindow):
 
     def __init__(self):
@@ -16,17 +30,29 @@ class Window(QtGui.QMainWindow):
         self.setGeometry(50, 50, 500, 300)
         self.setWindowTitle("GS")
         self.cmd_zero_btn()
-
+        self.cmd_buzz_btn()
+        self.cmd_release_btn()
         self.show()
-
 
     def cmd_zero_btn(self):
         btn = QtGui.QPushButton("Reset Save Data", self)
-        btn.clicked.connect(self.send_cmd)
+        btn.clicked.connect(lambda: self.send_cmd(CMD_RESET))
+        btn.resize(200, 50)
 
-    def send_cmd(self):
-        print('here')
-        xbee.write('z')
+    def cmd_buzz_btn(self):
+        btn = QtGui.QPushButton("Sounds Buzzer", self)
+        btn.clicked.connect(lambda: self.send_cmd(CMD_BUZZER))
+        btn.resize(200, 50)
+        btn.move(0, 50)
+
+    def cmd_release_btn(self):
+        btn = QtGui.QPushButton("RELEASE", self)
+        btn.clicked.connect(lambda: self.send_cmd(CMD_RELEASE))
+        btn.resize(200, 50)
+        btn.move(0, 100)
+
+    def send_cmd(self, cmd):
+        xbee.write(cmd)
 
 
 
